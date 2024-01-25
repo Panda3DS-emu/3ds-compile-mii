31f59aaef3f39e212f41ab7c3034c4203c05d954
wheremyfoodat <44909372+wheremyfoodat@users.noreply.github.com>
2024-01-25 03:31:50 +0200

    Update main.c

--- tests/mii-selector/source/main.c
+++ tests/mii-selector/source/main.c
@@ -5,11 +5,12 @@ int main(int argc, char **argv)
 {
 	gfxInitDefault();
 	consoleInit(GFX_TOP, NULL);
+	consoleDebugInit(debugDevice_SVC);
 
-	printf("Mii selector demo.\n");
-	printf("Press A to bring up Mii selector with default settings.\n");
-	printf("Press B to bring up Mii selector with custom settings.\n");
-	printf("Press START to exit.\n");
+	fprint(stderr, "Mii selector demo.\n");
+	fprint(stderr, "Press A to bring up Mii selector with default settings.\n");
+	fprint(stderr, "Press B to bring up Mii selector with custom settings.\n");
+	fprint(stderr, "Press START to exit.\n");
 
 	while (aptMainLoop())
 	{
@@ -63,24 +64,24 @@ int main(int argc, char **argv)
 			{
 				if (!msRet.no_mii_selected)
 				{
-					printf("A Mii was selected.\n");
+					fprint(stderr, "A Mii was selected.\n");
 					miiSelectorReturnGetName(&msRet, miiname, sizeof(miiname));
 					miiSelectorReturnGetAuthor(&msRet, miiauthor, sizeof(miiauthor));
-					printf("Name: %s\n", miiname);
-					printf("Author: %s\n", miiauthor);
-					printf("Birthday: Month-%d/Day-%d\n", msRet.mii.mii_details.bday_month, msRet.mii.mii_details.bday_day);
-					printf("Sex: %d\n", msRet.mii.mii_details.sex);
-					printf("Color: %d\n", msRet.mii.mii_details.shirt_color);
-					printf("Favorite: %d\n", msRet.mii.mii_details.favorite);
+					fprint(stderr, "Name: %s\n", miiname);
+					fprint(stderr, "Author: %s\n", miiauthor);
+					fprint(stderr, "Birthday: Month-%d/Day-%d\n", msRet.mii.mii_details.bday_month, msRet.mii.mii_details.bday_day);
+					fprint(stderr, "Sex: %d\n", msRet.mii.mii_details.sex);
+					fprint(stderr, "Color: %d\n", msRet.mii.mii_details.shirt_color);
+					fprint(stderr, "Favorite: %d\n", msRet.mii.mii_details.favorite);
 					// Keep in mind that not all things align with their order in Mii Maker.
 					// https://www.3dbrew.org/wiki/Mii#Mii_values
-					printf("Eyebrow: %d\n", msRet.mii.eyebrow_details.style);
-					printf("Eyebrow color: %d\n", msRet.mii.eyebrow_details.color);
-					printf("Nose: %d\n", msRet.mii.nose_details.style);
+					fprint(stderr, "Eyebrow: %d\n", msRet.mii.eyebrow_details.style);
+					fprint(stderr, "Eyebrow color: %d\n", msRet.mii.eyebrow_details.color);
+					fprint(stderr, "Nose: %d\n", msRet.mii.nose_details.style);
 				} else
-					printf("No Mii was selected.\n");
+					fprint(stderr, "No Mii was selected.\n");
 			} else
-				printf("Return checksum invalid.\n");
+				fprint(stderr, "Return checksum invalid.\n");
 		}
 
 		// Flush and swap framebuffers
